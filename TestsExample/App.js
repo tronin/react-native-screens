cf1f42617464500dba92fda88af22c0f0f7d25a0
WoLewicki <wojciech.lewicki@swmansion.com>
2020-12-28 14:33:44 +0100

    feat: add prop to android and the test example

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -23,6 +23,7 @@ import Test691 from './src/Test691';
 import Test702 from './src/Test702';
 import Test706 from './src/Test706';
 import Test713 from './src/Test713';
+import Test761 from './src/Test761';
 
 enableScreens();
 
